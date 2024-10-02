5621bd5bfc12e1aed7186728bd4e8336d558773e
Brikeshmaurya <brikeshm01@gmail.com>
2024-10-02 07:59:01 +0530

    password validation

--- public/js/script.js
+++ public/js/script.js
@@ -13,16 +13,17 @@
     })
 })();
 
-// Password and Email validation
 document.getElementById('passwordForm').addEventListener('submit', function(e) {
+    let formIsValid = true; // Use this to keep track of the form's validity
+
     // Password validation
     const password = document.getElementById('password').value;
     const passwordErrorElement = document.getElementById('passwordError');
     const passwordRegex = /^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/;
 
     if (!passwordRegex.test(password)) {
-        e.preventDefault();
         passwordErrorElement.textContent = 'Password must be at least 8 characters long, contain at least one number, and one uppercase letter.';
+        formIsValid = false; // Set formIsValid to false if password is invalid
     } else {
         passwordErrorElement.textContent = '';
     }
@@ -33,9 +34,14 @@ document.getElementById('passwordForm').addEventListener('submit', function(e) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
     if (!emailRegex.test(email)) {
-        e.preventDefault();
         emailErrorElement.textContent = 'Please enter a valid email address.';
+        formIsValid = false; // Set formIsValid to false if email is invalid
     } else {
         emailErrorElement.textContent = '';
+    }
+
+    // Prevent form submission if there are any validation errors
+    if (!formIsValid) {
+        e.preventDefault();
     }
 });
