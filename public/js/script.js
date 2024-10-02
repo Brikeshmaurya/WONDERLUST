3005da454fda8ab14fe24418914eb21ced7f1e8d
Brikeshmaurya <brikeshm01@gmail.com>
2024-10-02 08:27:31 +0530

    update scrip.js

--- public/js/script.js
+++ public/js/script.js
@@ -15,7 +15,6 @@
 
 //Validation
 document.getElementById('passwordForm').addEventListener('submit', function(e) {
-    let formIsValid = true;
 
     const password = document.getElementById('password').value;
     const passwordErrorElement = document.getElementById('passwordError');
@@ -24,7 +23,6 @@ document.getElementById('passwordForm').addEventListener('submit', function(e) {
 
     if (!passwordRegex.test(password)) {
         passwordErrorElement.textContent = 'Password must be at least 8 characters long, contain at least one number, and one uppercase letter.';
-        formIsValid = false;
     }
 
     const email = document.getElementById('email').value;
@@ -33,10 +31,6 @@ document.getElementById('passwordForm').addEventListener('submit', function(e) {
     emailErrorElement.textContent = '';
     if (!emailRegex.test(email)) {
         emailErrorElement.textContent = 'Please enter a valid email address.';
-        formIsValid = false;
     }
 
-    if (!formIsValid) {
-        e.preventDefault();
-    }
 });
